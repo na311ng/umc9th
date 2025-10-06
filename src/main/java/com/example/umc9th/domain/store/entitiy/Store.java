4f11add4ec745ea729a6221ad9e9f8fec4c98260
nayoung <dlskdud0311@naver.com>
2025-10-06 21:34:29 +0900

    feat:Store, location 엔티티 생성 및 연관관계 설정

--- src/main/java/com/example/umc9th/domain/store/entitiy/Store.java
+++ src/main/java/com/example/umc9th/domain/store/entitiy/Store.java
@@ -1,15 +1,29 @@
 package com.example.umc9th.domain.store.entitiy;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Table(name = "store")
 public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "boss_number", nullable = false, length = 20)
+    private String bossNumber;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
 }
