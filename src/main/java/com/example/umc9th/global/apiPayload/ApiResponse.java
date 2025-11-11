d6d6a469ee6c96814f6048f83c59c3f7e04ef4fe
nayoung <dlskdud0311@naver.com>
2025-11-11 18:16:20 +0900

    fix: api응답 수정

--- src/main/java/com/example/umc9th/global/apiPayload/ApiResponse.java
+++ src/main/java/com/example/umc9th/global/apiPayload/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.example.umc9th.global.apiPayload;
 
 import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc9th.global.apiPayload.code.BaseSuccessCode;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
@@ -24,7 +25,13 @@ public class ApiResponse<T> {
     private T result;
 
     // 성공한 경우 (result 포함)
-
+    public static<T> ApiResponse<T> onSuccess(BaseSuccessCode code, T result){
+        return new ApiResponse<>(true, code.getCode(), code.getMessage(), result);
+    }
+    // 성공한 경우 (result 포함 X)
+    public static<T> ApiResponse<T> onSuccess(BaseSuccessCode code){
+        return new ApiResponse<>(true, code.getCode(), code.getMessage(), null);
+    }
     // 실패한 경우 (result 포함)
     public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
         return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
