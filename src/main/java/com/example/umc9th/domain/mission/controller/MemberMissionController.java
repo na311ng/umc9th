633458c66d6413e2fe605481068a690a485966d7
nayoung <dlskdud0311@naver.com>
2025-11-11 23:37:39 +0900

    fix: 목록 조회 api 응답 통일

--- src/main/java/com/example/umc9th/domain/mission/controller/MemberMissionController.java
+++ src/main/java/com/example/umc9th/domain/mission/controller/MemberMissionController.java
@@ -1,7 +1,9 @@
 package com.example.umc9th.domain.mission.controller;
 
-import com.example.umc9th.domain.mission.dto.res.MemberMissionResponse;
+import com.example.umc9th.domain.mission.dto.res.MemberMissionResDTO;
 import com.example.umc9th.domain.mission.service.MemberMissionService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +18,18 @@ public class MemberMissionController {
     private final MemberMissionService memberMissionService;
 
     @GetMapping("/my")
-    public Page<MemberMissionResponse> getMyMissions(
+    public ApiResponse<MemberMissionResDTO.MemberMissionPageDTO> getMyMissions(
+            @RequestParam(required = false) String status,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ){
         Long loginMemberId = 1L; // TODO: 실제는 JWT 토큰에서 추출
-        return memberMissionService.getMyMissions(loginMemberId, page, size);
+        Page<MemberMissionResDTO.MemberMissionInfoDTO> missionsPage =
+                memberMissionService.getMyMissions(loginMemberId, status, page, size);
+
+        return ApiResponse.onSuccess(
+                GeneralSuccessCode.OK,
+                MemberMissionResDTO.MemberMissionPageDTO.fromPage(missionsPage)
+        );
     }
 }
--- src/main/java/com/example/umc9th/domain/mission/service/MemberMissionService.java
+++ src/main/java/com/example/umc9th/domain/mission/service/MemberMissionService.java
@@ -1,18 +1,20 @@
 package com.example.umc9th.domain.mission.service;
 
-import com.example.umc9th.domain.mission.dto.res.MemberMissionResponse;
+import com.example.umc9th.domain.mission.dto.res.MemberMissionResDTO;
 import com.example.umc9th.domain.mission.repository.MemberMissionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberMissionService {
     private final MemberMissionRepository memberMissionRepository;
 
-    public Page<MemberMissionResponse> getMyMissions(Long memberId, int page, int size) {
-        return memberMissionRepository.findeMemberMissionByMemeberId(memberId, PageRequest.of(page, size));
+    public Page<MemberMissionResDTO.MemberMissionInfoDTO> getMyMissions(Long memberId, String status, int page, int size) {
+        return memberMissionRepository.findeMemberMissionByMemeberId(memberId, status, PageRequest.of(page, size));
     }
 }
