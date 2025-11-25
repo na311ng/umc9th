bf6c2732e278c51d3c850601d139cbfcb4b9a380
nayoung <dlskdud0311@naver.com>
2025-11-26 04:37:33 +0900

    refactor: MemberMissionController /missions/my api 응답 수정

--- src/main/java/com/example/umc9th/domain/mission/controller/MemberMissionController.java
+++ src/main/java/com/example/umc9th/domain/mission/controller/MemberMissionController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.mission.controller;
 
 import com.example.umc9th.domain.mission.dto.MemberMissionResponse;
 import com.example.umc9th.domain.mission.service.MemberMissionService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberMissionController {
     private final MemberMissionService memberMissionService;
 
+    // 나의 미션 목록 조회
     @GetMapping("/my")
-    public Page<MemberMissionResponse> getMyMissions(
+    public ApiResponse<Page<MemberMissionResponse>>  getMyMissions(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ){
         Long loginMemberId = 1L; // TODO: 실제는 JWT 토큰에서 추출
-        return memberMissionService.getMyMissions(loginMemberId, page, size);
+        Page <MemberMissionResponse> missions = memberMissionService.getMyMissions(loginMemberId, page, size);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, missions);
     }
 }
