c6a2d224c166d2e0e449fe462af14d4f95f3b7e8
nayoung <dlskdud0311@naver.com>
2025-11-26 04:42:24 +0900

    refactor: MissionController /missions api 응답 수정

--- src/main/java/com/example/umc9th/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/umc9th/domain/mission/controller/MissionController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.mission.controller;
 
 import com.example.umc9th.domain.mission.dto.MissionHomeResponse;
 import com.example.umc9th.domain.mission.service.MissionService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,12 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MissionController {
     private final MissionService missionService;
 
+    // 지역별 미션 조회
     @GetMapping
-    public Page<MissionHomeResponse> getMissionsByLocation(
+    public ApiResponse<Page<MissionHomeResponse>>  getMissionsByLocation(
             @RequestParam Long locationId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        return missionService.getMissionsByLocation(locationId, PageRequest.of(page, size));
+        Page<MissionHomeResponse> missions = missionService.getMissionsByLocation(locationId, PageRequest.of(page, size));
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, missions);
     }
 }
